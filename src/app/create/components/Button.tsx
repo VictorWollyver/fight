1cbc115b3dfaa1a0fc9f5976670090fed1476352
Renan Gimenez <renangiimenez@gmail.com>
2024-10-20 03:11:39 -0300

    style: update Button component

--- src/app/create/components/Button.tsx
+++ src/app/create/components/Button.tsx
@@ -1,13 +1,24 @@
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  baseColor?: string;
+  theme?: "yellow" | "green" | "blue";
   children: React.ReactNode;
 }
 
-const Button = ({ baseColor, children, ...rest }: ButtonProps) => {
+const Button = ({ theme, children, ...rest }: ButtonProps) => {
+  const variantes = {
+    blue: "text-blue border-blue hover:bg-blue hover:border-strokeBlue hover:shadow-lightShadow hover:shadow-shadowBlue",
+    yellow:
+      "text-yellow border-yellow hover:bg-yellow hover:border-strokeYellow hover:shadow-lightShadow hover:shadow-shadowYellow",
+    green:
+      "text-green border-green hover:bg-green hover:border-strokeGreen hover:shadow-lightShadow hover:shadow-shadowGreen",
+  };
+
   return (
     <button
-      style={{ borderColor: baseColor, color: baseColor }}
-      className={`h-16 min-w-16 w-full flex justify-center items-center text-3xl hover:text-black border-[5px] rounded-xl`}
+      className={`
+        h-16 min-w-16 w-full flex justify-center items-center
+        text-3xl border-[5px] button rounded-xl transition-all 
+        hover:text-white ${theme && variantes[theme]}
+      `}
       {...rest}
     >
       {children}
