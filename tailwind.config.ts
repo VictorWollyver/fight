0213990e55092fe5f38b22522e31ac16c5612876
Renan Gimenez <renangiimenez@gmail.com>
2024-10-20 03:11:17 -0300

    style: create color variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,12 +6,27 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  
+
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+
+        green: "var(--green)",
+        yellow: "var(--yellow)",
+        blue: "var(--blue)",
+
+        strokeGreen: "var(--border-green)",
+        strokeYellow: "var(--border-yellow)",
+        strokeBlue: "var(--border-blue)",
+
+        shadowGreen: "var(--shadow-green)",
+        shadowYellow: "var(--shadow-yellow)",
+        shadowBlue: "var(--shadow-blue)",
+      },
+      boxShadow: {
+        lightShadow: "0 0 10px 4px rgba(0, 0, 0, 0.3)",
       },
     },
   },
